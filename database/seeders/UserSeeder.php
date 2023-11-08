cabfd32b92c7d21f33a7fea945f220cd8489b31b
winkersco <smmehdisharifi@gmail.com>
2023-11-08 17:19:28 +0330

    fix: change UserSeeder to fill username with default value

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::factory()->create([
-            'name' => 'parspack',
+            'username' => 'parspack',
         ]);
     }
 }
