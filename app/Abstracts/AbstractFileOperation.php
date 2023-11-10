2898e295dc7ddf4dbe6498b5edb6bbd308d40830
winkersco <smmehdisharifi@gmail.com>
2023-11-10 16:21:46 +0330

    docs: write PSR-5 comments for AbstractFileOperation

--- app/Abstracts/AbstractFileOperation.php
+++ app/Abstracts/AbstractFileOperation.php
@@ -4,12 +4,24 @@ namespace App\Abstracts;
 
 use Symfony\Component\Process\Process;
 
+/**
+ * Abstract class representing a file operation.
+ *
+ * @package App\Abstracts
+ */
 abstract class AbstractFileOperation
 {
     protected $key;
     protected $value;
     protected $filepath;
 
+    /**
+     * Constructor for AbstractFileOperation.
+     *
+     * @param string $key      The key.
+     * @param string $value    The value.
+     * @param string $filepath The filepath.
+     */
     public function __construct($key, $value, $filepath)
     {
         $this->key = escapeshellarg($key);
@@ -17,6 +29,9 @@ abstract class AbstractFileOperation
         $this->filepath = escapeshellarg($filepath);
     }
 
+    /**
+     * Execute the file operation.
+     */
     public function execute()
     {
         $command = $this->getCommand();
@@ -24,5 +39,10 @@ abstract class AbstractFileOperation
         $process->run();
     }
 
+    /**
+     * Get the command for the file operation.
+     *
+     * @return string The command.
+     */
     protected abstract function getCommand();
 }
