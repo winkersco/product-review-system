73569e8388cd84e783e128ade402dc422e2f2a5c
winkersco <smmehdisharifi@gmail.com>
2023-11-10 16:17:50 +0330

    feat: implement updating product_comments file + refactor CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Factories\AppendFileOperationFactory;
+use App\Factories\ReplaceFileOperationFactory;
 use App\Helpers\ApiResponse;
 use App\Http\Requests\StoreCommentRequest;
-use App\Http\Requests\UpdateCommentRequest;
 use App\Http\Resources\CommentResource;
 use App\Models\Comment;
 use App\Models\Product;
@@ -23,17 +24,49 @@ class CommentController extends Controller
         $product = Product::firstOrCreate(['name' => $request->input('product_name')]);
         $user = auth()->user();
 
-        if (Comment::userID($user->id)->productID($product->id)->count() > 2) {
+        if ($this->hasReachedCommentLimit($user, $product)) {
             return ApiResponse::createResponse('You have reached the maximum limit of comments for this product.', null, Response::HTTP_FORBIDDEN);
         }
 
         $comment = $product->comments()->create([
             'comment' => $request->input('comment'),
             'user_id' => $user->id
-        ]);
+        ])->load(['user', 'product']);
 
-        $comment->load(['user', 'product']);
+        $this->updateProductCommentFile($product);
 
         return ApiResponse::createResponse('Comment created successfully.', CommentResource::make($comment), Response::HTTP_CREATED);
+    }
+
+    /**
+     * Check if the user has reached the maximum comment limit for the product.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Product  $product
+     * @return bool
+     */
+    private function hasReachedCommentLimit($user, $product)
+    {
+        return Comment::userID($user->id)->productID($product->id)->count() >= 2;
+    }
+
+    /**
+     * Update the product comment file based on the product's state.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    private function updateProductCommentFile($product)
+    {
+        $key = $product->name;
+        $value = $product->comments->count() + 1;
+        $filepath = config('constants.product_comments_filepath');
+
+        if ($product->wasRecentlyCreated) {
+            $factory = new AppendFileOperationFactory($key, $value, $filepath);
+        } else {
+            $factory = new ReplaceFileOperationFactory($key, $value, $filepath);
+        }
+        $factory->execute();
     }
 }
---
+++ config/constants.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'product_comments_filepath' => base_path('product_comments')
+];
