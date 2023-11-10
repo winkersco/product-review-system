3b2a2ae9ac4eda3018bf1476da1e08486ed5972c
winkersco <smmehdisharifi@gmail.com>
2023-11-10 16:30:00 +0330

    fix: fix misspell in ApiResponse

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Response;
 
 class ApiResponse
 {
-    public static function createResponse($message = null, $data = null, $statusCode = Response::HTTsP_OK)
+    public static function createResponse($message = null, $data = null, $statusCode = Response::HTTP_OK)
     {
         $success = ($statusCode >= 200 && $statusCode < 400);
         return response()->json([
